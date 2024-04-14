3b72b564996882efb0ffc0f443dfff1a0e7b0bc4
Trish <roguemacaroon@gmail.com>
2024-04-14 20:50:05 -0300

    tidy up js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Get the button
 let topBtn = document.getElementById("topBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -12,7 +10,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
